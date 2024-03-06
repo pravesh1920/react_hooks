c357a59e6c5d39dcb7d8857d1c198116f82a1d3d
pravesh1920 <61235397+pravesh1920@users.noreply.github.com>
2024-03-06 06:43:13 +0000

    UseState Hooks

--- src/01_useState/index.js
+++ src/01_useState/index.js
@@ -6,22 +6,46 @@ function Counter () {
     // }
     /** useState return array with 2 values, first value is current state and second value is function for update state */
     //const arry = useSate(4)
-    function counterInitial () {
-        console.log('Run function');
-        return 4;
-    }
-    const [count, setCount] = useState(counterInitial);
+    // function counterInitial () {
+    //     console.log('Run function');
+    //     return 4;
+    // }
+    // const [count, setCount] = useState(counterInitial);
+    // const [state, setState] = useState({count:4, theme: 'blue'});
+    const [count, setCount] = useState(4);
+    const [theme, setTheme] = useState('blue');
+    // const count = state.count;
+    // const theme = state.theme;
     function decrementCount () {
+        // setState(prevState => {
+            // this is overriding all our state
+            // return {count: prevState.count -1}
+            /* Here we need to use spread operator so it will not override
+               complete object 
+            */
+            //return {...prevState, count:prevState.count - 1}
+
+        // })
         setCount(preCount => preCount - 1); 
-        setCount(preCount => preCount - 1);
+        setTheme('red');
+        // setCount(preCount => preCount - 1);
     }
     function incrementCount() {
+        //setState(prevState => {
+            // this is overriding all our state
+            // return {count: prevState.count +1}
+            /* Here we need to use spread operator so it will not override
+               complete object 
+            */
+            // return {...prevState, count: prevState.count +1}
+        //})
         setCount(preCount => preCount + 1);
     }
     return(
         <div style={{margin:20}}>
             <button onClick={decrementCount}> - </button>
             <span>{count}</span>
+            <span>{theme}</span>
             <button onClick={incrementCount}> + </button>
         </div>
     );
